d1ae2a41635563f3ea927e35e725bc961c43ddbe
ganesh.kalje@gmail.com <ganesh.kalje@gmail.com>
2018-09-01 08:54:05 +0530

    change for list

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import {FormsModule} from '@angular/forms';
 import { AdminRoutingModule } from './admin-routing.module';
 
 import { AdminComponent } from './admin.component';
@@ -10,9 +10,11 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
 import {CelebrityService} from './service/celebrity.service';
 
+
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     HttpClientModule,
     AdminRoutingModule,
     TranslateModule
--- src/app/admin/celebrity/celebrity.module.ts
+++ src/app/admin/celebrity/celebrity.module.ts
@@ -4,13 +4,27 @@ import { CommonModule } from '@angular/common';
 import { CelebrityRoutingModule } from './celebrity-routing.module';
 import { ListComponent } from './list/list.component';
 import {TableModule} from 'primeng/table';
+import {AddComponent} from './add/add.component';
+import {PanelModule} from "primeng/panel";
+import {DropdownModule, InputTextareaModule, InputTextModule, TabViewModule} from "primeng/primeng";
+import {ButtonModule} from "primeng/button";
+import {MessageModule} from "primeng/message";
+import {ToastModule} from "primeng/toast";
 
 @NgModule({
   imports: [
     CommonModule,
     CelebrityRoutingModule,
-    TableModule
+    TableModule,
+      ToastModule,
+      MessageModule,
+      PanelModule,
+      DropdownModule,
+      InputTextModule,
+      InputTextareaModule,
+      ButtonModule,
+      TabViewModule
   ],
-  declarations: [ListComponent]
+  declarations: [ListComponent, AddComponent]
 })
 export class CelebrityModule { }

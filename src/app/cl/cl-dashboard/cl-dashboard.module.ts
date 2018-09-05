32b20c20288c63bc31b6d861d1886d9786288a56
Kalje <710241@cognizant.com>
2018-09-05 19:49:54 +0530

    added dashboard

--- src/app/cl/cl-dashboard/cl-dashboard.module.ts
+++ src/app/cl/cl-dashboard/cl-dashboard.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClDashboardRoutingModule } from './cl-dashboard-routing.module';
+import {
+    TimelineComponent,
+    NotificationComponent,
+    ChatComponent
+} from './components';
 
 @NgModule({
   imports: [
     CommonModule,
     ClDashboardRoutingModule
   ],
-  declarations: []
+  declarations: [TimelineComponent, NotificationComponent,
+        ChatComponent]
 })
 export class ClDashboardModule { }

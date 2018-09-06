e317f6c495f2a6b67ac664239f45cb0a15d36298
Kalje <710241@cognizant.com>
2018-09-06 16:30:01 +0530

    change for dashboard

--- src/app/cl/celebrity.component.ts
+++ src/app/cl/celebrity.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-celebrity',
-  templateUrl: './celebrity.component.html'
+  templateUrl: './celebrity.component.html',
+  styleUrls: ['./celebrity.component.scss']
 })
 export class CelebrityComponent implements OnInit {
 

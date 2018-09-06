34c375d4487dbf772e2ccb387640aab5d178fe83
Kalje <710241@cognizant.com>
2018-09-06 16:24:08 +0530

    change for customoers

--- src/app/cl/cl-dashboard/cl-dashboard-routing.module.ts
+++ src/app/cl/cl-dashboard/cl-dashboard-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CldashboardComponent } from './cldashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+        path: '', component: CldashboardComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/cl/cl-dashboard/cl-dashboard.module.ts
+++ src/app/cl/cl-dashboard/cl-dashboard.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbCarouselModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ClDashboardRoutingModule } from './cl-dashboard-routing.module';
+import { StatModule } from '../../shared';
 import {
     TimelineComponent,
     NotificationComponent,
     ChatComponent
 } from './components';
+import { CldashboardComponent } from './cldashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ClDashboardRoutingModule
+    NgbCarouselModule.forRoot(),
+    NgbAlertModule.forRoot(),
+    ClDashboardRoutingModule,
+    StatModule
   ],
-  declarations: [TimelineComponent, NotificationComponent,
-        ChatComponent]
+  declarations: [TimelineComponent, NotificationComponent, ChatComponent, CldashboardComponent]
 })
 export class ClDashboardModule { }

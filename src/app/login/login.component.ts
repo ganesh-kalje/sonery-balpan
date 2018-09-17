e829c32a827d2be91ae378e95e61a8b192a61c2d
ganesh.kalje@gmail.com <ganesh.kalje@gmail.com>
2018-09-17 09:24:54 +0530

    change for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,7 +12,7 @@ import {MessageService} from 'primeng/api';
 })
 export class LoginComponent implements OnInit {
     constructor(public router: Router, private messageService: MessageService) {}
-
+    model: any = {};
     ngOnInit() {
         setTimeout(() => {
             this.messageService.add({severity:'success', summary:'Service Message', detail:'Via MessageService'});
@@ -21,7 +21,7 @@ export class LoginComponent implements OnInit {
         this.messageService.add({severity:'success', summary:'Service Message', detail:'Via MessageService'});
     }
 
-    onLoggedin(username, password) {
+    onLoggedIn(username, password) {
         if(username === '9762029258' && password === 'admin') {
     		this.router.navigate(['dashboard']);
     	} else {
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import {ToastModule} from 'primeng/toast';
 
 @NgModule({
-    imports: [CommonModule, LoginRoutingModule, ToastModule],
+    imports: [CommonModule, FormsModule,  LoginRoutingModule, ToastModule],
     declarations: [LoginComponent]
 })
 export class LoginModule {}
